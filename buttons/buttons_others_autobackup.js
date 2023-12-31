78be68acfb2fcef317c359d28dc1385cae929a73
regorxxx <regorxxx@protonmail.com>
2023-12-31 15:10:45 +0100

    - File formatting: error checking Regular Expressions on user input.

--- buttons/buttons_others_autobackup.js
+++ buttons/buttons_others_autobackup.js
@@ -1,35 +1,50 @@
 ﻿'use strict';
-//09/12/23
+//30/12/23
 
+/* global menu_panelProperties:readable */
 include('..\\helpers\\helpers_xxx.js');
+/* global globFonts:readable, MK_SHIFT:readable, VK_SHIFT:readable, folders:readable */
 include('..\\helpers\\buttons_xxx.js');
-include('..\\helpers\\buttons_xxx_menu.js');
-include('..\\helpers\\helpers_xxx_input.js');
+/* global getUniquePrefix:readable, buttonsBar:readable, addButton:readable, themedButton:readable */
 include('..\\helpers\\menu_xxx.js');
+/* global _menu:readable  */
 include('..\\helpers\\menu_xxx_extras.js');
+/* global _createSubMenuEditEntries:readable */
+include('..\\helpers\\helpers_xxx_prototypes.js');
+/* global isBoolean:readable, isInt:readable, isJSON:readable, isString:readable */
+include('..\\helpers\\helpers_xxx_file.js');
+/* global _open:readable, utf8:readable, _jsonParseFileCheck:readable, _isFile:readable, _explorer:readable */
+include('..\\helpers\\helpers_xxx_UI.js');
+/* global _gdiFont:readable, _gr:readable, _scale:readable, chars:readable */
+include('..\\helpers\\helpers_xxx_properties.js');
+/* global setProperties:readable, getPropertiesPairs:readable, checkProperty:readable, overwriteProperties:readable */
+include('..\\helpers\\helpers_xxx_input.js');
+/* global Input:readable */
 include('..\\main\\autobackup\\autobackup.js');
-var prefix = 'bak';
-var version = '1.0.0';
+/* global AutoBackup:readable */
 
-try {window.DefineScript('AutoBackup Button', {author:'regorxxx', version, features: {drag_n_drop: false}});} catch (e) {/* console.log('AutoBackup Button loaded.'); */} //May be loaded along other buttons
+var prefix = 'bak'; // NOSONAR[global]
+var version = '1.0.0'; // NOSONAR[global]
+
+try {window.DefineScript('AutoBackup Button', {author:'regorxxx', version, features: {drag_n_drop: false}});} catch (e) { /* May be loaded along other buttons */ }
 
 prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
 
-var newButtonsProperties = { //You can simply add new properties here
+var newButtonsProperties = { // NOSONAR[global]
 	iPlaying:		['While playing (every X min, 0 = off)', 60, {func: isInt}, 60],
 	iStop:			['When playback stops (after X min, 0 = off)', 5, {func: isInt}, 5],
 	iInterval:		['Always, since last autosave (every X min, 0 = off)', 30, {func: isInt}, 30],
 	iStart:			['On startup (after X min, 0 = off)', 1, {func: isInt}, 1],
 	iTrack:			['Every X tracks (0 = off)', 0, {func: isInt}, 0],
 	iClose:			['On Foobar2000 exit (after X secs, 0 = off)', 20, {func: isInt}, 20],
-	files:			['Files and Folders mask', 
+	files:			['Files and Folders mask',
 		JSON.stringify([
 			// Foobar folders
 			{name: 'Main config', path: 'configuration'},
 			{name: 'Playlists (1)', path: 'playlists*'},
 			{name: 'Statistics (1)', path: 'index-data'},
 			{name: 'VST config', path: 'vst-presets'},
-			 // Foobar Files
+			// Foobar Files
 			{name: 'Theme', path: 'theme.fth'},
 			{name: 'Statistics (2)', path: 'PlaybackStatistics.dat'},
 			{name: 'Library', path: 'database.dat'},
@@ -52,9 +67,9 @@ var newButtonsProperties = { //You can simply add new properties here
 		{func: isJSON}],
 	outputPath:		['Directory to store backup files', 'autobackup\\autobackup.', {func: isString}, 'autobackup\\autobackup.'],
 	iBackups:		['Number of backups to keep', 8, {func: isInt}, 8],
-	backupFormat:	['Backups file format replacers', 
+	backupFormat:	['Backups file format replacers',
 		JSON.stringify([
-			{name: 'Remove \\- :,', regex: '[\- :,]', flag: 'g', replacer: ''}, 
+			{name: 'Remove \\- :,', regex: '[\\- :,]', flag: 'g', replacer: ''},
 			{name: 'Replace T', regex: 'T', flag: 'g', replacer: '-'}
 		]), {func: isJSON}
 	],
@@ -63,7 +78,8 @@ var newButtonsProperties = { //You can simply add new properties here
 	bIconMode:		['Icon-only mode?', false, {func: isBoolean}, false],
 	bStartActive:	['Always active on startup', true, {func: isBoolean}, true],
 	bHeadlessMode:	['Headless mode', false, {func: isBoolean}, false],
-	backupsMaxSize:	['Max size of backup files', 2000, {func: isInt}, 2000],
+	backupsMaxSize:	['Max size of backup files (MB)', 2000, {func: isInt}, 2000],
+	minDriveSize:	['Min free space on drive (MB)', 4000, {func: isInt}, 4000],
 };
 newButtonsProperties.files.push(newButtonsProperties.files[1]);
 newButtonsProperties.backupFormat.push(newButtonsProperties.backupFormat[1]);
@@ -74,7 +90,7 @@ buttonsBar.list.push(newButtonsProperties);
 
 addButton({
 	AutoBackup: new themedButton({x: 0, y: 0, w: _gr.CalcTextWidth('AutoBackup', _gdiFont(globFonts.button.name, globFonts.button.size * buttonsBar.config.scale)) + 25 * _scale(1, false) /_scale(buttonsBar.config.scale), h: 22}, 'AutoBackup', function (mask) {
-		if ((MK_SHIFT & mask) === MK_SHIFT) {
+		if ((MK_SHIFT & mask) === MK_SHIFT) { // NOSONAR [bitwise]
 			this.autoBackup.forceBackup();
 		} else {
 			// Menu
@@ -89,8 +105,8 @@ addButton({
 				['iPlaying', 'iStop', 'iInterval', 'iStart', 'iTrack', 'iClose'].forEach((key) => {
 					const value = this.buttonsProperties[key][1];
 					const entryText = this.buttonsProperties[key][0].replace(/[a-zA-Z]*[0-9]*_*[0-9]*\./,'') + '\t[' + value + ']';
-					const unit = key === 'iTrack' 
-						? 'tracks' 
+					const unit = key === 'iTrack'
+						? 'tracks'
 						: key === 'iClose'
 							? 'seconds'
 							: 'minutes';
@@ -108,8 +124,8 @@ addButton({
 			_createSubMenuEditEntries(menu, void(0), {
 				subMenuName: 'Files and folders to backup...',
 				name: 'AutoBackup',
-				list: this.autoBackup.files, 
-				defaults: JSON.parse(this.buttonsProperties.files[3]), 
+				list: this.autoBackup.files,
+				defaults: JSON.parse(this.buttonsProperties.files[3]),
 				input : () => {
 					const entry = {
 						path: Input.string(
@@ -145,19 +161,19 @@ addButton({
 				_createSubMenuEditEntries(menu, menuName, {
 					subMenuName: 'Backup file formatting...',
 					name: 'AutoBackup',
-					list: this.autoBackup.backupFormat, 
-					defaults: JSON.parse(this.buttonsProperties.backupFormat[3]), 
+					list: this.autoBackup.backupFormat,
+					defaults: JSON.parse(this.buttonsProperties.backupFormat[3]),
 					input : () => {
 						const entry = {
 							...(Input.json(
-								'object', JSON.stringify({regex: '[\- :,]', flag: 'g', replacer: '_'}),
+								'object', JSON.stringify({regex: '[\\- :,]', flag: 'g', replacer: '_'}),
 								'Enter regex and replacer:\n' +
-								'Ex: ' + JSON.stringify({regex: '[\- :,]', flag: 'g', replacer: '_'})
-								, 'AutoBackup' , JSON.stringify({regex: '[\- :,]', flag: 'g', replacer: '_'}), void(0), true
+								'Ex: ' + JSON.stringify({regex: '[\\- :,]', flag: 'g', replacer: '_'})
+								, 'AutoBackup' , JSON.stringify({regex: '[\\- :,]', flag: 'g', replacer: '_'}), void(0), true
 							) || {})
 						};
-						if (!entry.hasOwnProperty('regex') || !entry.hasOwnProperty('flag') || !entry.hasOwnProperty('replacer')) {return;}
-						try {new RegExp(curr.regex, curr.flag)} catch (e) {return;}
+						if (!Object.hasOwn(entry, 'regex') || !Object.hasOwn(entry, 'flag') || !Object.hasOwn(entry, 'replacer')) {return;}
+						try {new RegExp(entry.regex, entry.flag);} catch (e) {return;}
 						return entry;
 					},
 					bNumbered: true,
@@ -188,6 +204,18 @@ addButton({
 						if (!checkProperty(this.buttonsProperties.backupsMaxSize, input)) {return;} // Apply properties check which should be personalized for input value
 						this.buttonsProperties.backupsMaxSize[1] = input;
 						this.autoBackup.backupsMaxSize = input * 1024 * 1024;
+						overwriteProperties(this.buttonsProperties);
+					}});
+				}
+				{
+					const value = this.buttonsProperties.minDriveSize[1];
+					const entryText = this.buttonsProperties.minDriveSize[0].replace(/[a-zA-Z]*[0-9]*_*[0-9]*\./,'') + '\t[' + value + ' MB]';
+					menu.newEntry({menuName, entryText, func: () => {
+						const input = Input.number('int', value, 'Enter MB' + ':\n(0 = off)', 'AutoBackup', this.buttonsProperties.minDriveSize[3]);
+						if (input === null) {return;}
+						if (!checkProperty(this.buttonsProperties.minDriveSize, input)) {return;} // Apply properties check which should be personalized for input value
+						this.buttonsProperties.minDriveSize[1] = input;
+						this.autoBackup.minDriveSize = input * 1024 * 1024;
 						overwriteProperties(this.buttonsProperties);
 					}});
 				}
@@ -223,7 +251,7 @@ addButton({
 			menu.newEntry({entryText: 'Readme...', func: () => {
 				const readmeList = _isFile(folders.xxx + 'helpers\\readme\\buttons_list.json') ? _jsonParseFileCheck(folders.xxx + 'helpers\\readme\\buttons_list.json', 'Readme list', window.Name, utf8) : null;
 				if (readmeList) {
-					const readmeFile = readmeList.hasOwnProperty('buttons_others_autobackup.js') ? readmeList['buttons_others_autobackup.js'] : '';
+					const readmeFile = Object.hasOwn(readmeList, 'buttons_others_autobackup.js') ? readmeList['buttons_others_autobackup.js'] : '';
 					const readme = readmeFile.length ? _open(folders.xxx + 'helpers\\readme\\' + readmeFile, utf8) : '';
 					if (readme.length) {fb.ShowPopupMessage(readme, readmeFile);}
 					else {console.log(readmeFile + ' not found.');}
@@ -256,7 +284,8 @@ addButton({
 			iInterval: Number(newButtonsProperties.iInterval[1]) * 60000,
 			iStart: Number(newButtonsProperties.iStart[1]) * 60000,
 			iTrack: Number(newButtonsProperties.iTrack[1]),
-			iClose: Number(newButtonsProperties.iClose[1])
+			iClose: Number(newButtonsProperties.iClose[1]),
+			minDriveSize: Number(newButtonsProperties.minDriveSize[1]) * 1024 * 1024,
 		})
 	}, void(0), function() {
 		this.active = this.autoBackup.active = this.buttonsProperties.bStartActive[1] ? true : this.buttonsProperties.active[1];
