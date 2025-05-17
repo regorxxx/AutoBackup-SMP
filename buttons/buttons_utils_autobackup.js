b25113b2b6eca2bc86ed213ed1f43d02e2827171
regorxxx <regorxxx@protonmail.com>
2025-05-17 20:38:40 +0200

    Bump version

--- buttons/buttons_utils_autobackup.js
+++ buttons/buttons_utils_autobackup.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//17/03/25
+//17/05/25
 
 /* global menu_panelProperties:readable */
 include('..\\helpers\\helpers_xxx.js');
@@ -24,7 +24,7 @@ include('..\\main\\autobackup\\autobackup.js');
 /* global AutoBackup:readable */
 
 var prefix = 'bak'; // NOSONAR[global]
-var version = '2.4.0'; // NOSONAR[global]
+var version = '2.5.0'; // NOSONAR[global]
 
 try { window.DefineScript('AutoBackup Button', { author: 'regorxxx', version, features: { drag_n_drop: false } }); } catch (e) { /* May be loaded along other buttons */ } // eslint-disable-line no-unused-vars
 
